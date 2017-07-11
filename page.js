b12699e2a494198952cf6bc6a8d972f7c7859777
Wietske Visser <visser.wietske@gmail.com>
2017-07-11 22:15:10 +0200

    Text tweaks

--- page.js
+++ page.js
@@ -2,45 +2,49 @@ var data = {
 	courses: [
 		{
 			name: "Linear Algebra",
+			intro: "<p>These web lectures cover most topics of Linear Algebra (8 ECTS). You can post your questions in the comments sections on YouTube.</p><p><em>Coming soon!</em>There is also an online course in preparation (4 ECTS) which contains the first half of these web lectures, electronic exercises with feedback and the possibility to pose questions on forums. To access this course, you need a (free) <a href=\"https://moodle.org/\" target=\"_blank\">Moodle</a> account.</p>",
 			playlists: [
 				{
-					name: "Linear Algebra - vectors, lines and planes",
+					name: "1. Vectors, lines and planes",
 					id: "PLJMXXdEk8kMDKHXfYZycypaK2VFsQo25T"
 				},
 				{
-					name: "Linear Algebra - systems of linear equations",
+					name: "2. Systems of linear equations",
 					id: "PLJMXXdEk8kMBCcedEI8B13tdYBGCnUISg"
 				}
 			]
 		},
 		{
 			name: "Calculus",
+			intro: "Web lectures on selected topics of single variable calculus: introduction to functions, sequences, series and power series. What topic should be covered next? Post your ideas in the comments section of the <a href=\"https://www.youtube.com/watch?v=EGDbnmBpfRY\" target=\"_blank\">introduction video</a>!",
 			playlists: [
 				{
-					name: "Calculus - introduction to functions",
+					name: "1. Introduction to functions",
 					id: "PLJMXXdEk8kMD27qyxFL_d7Slkk0NpBAlh"
 				},
 				{
-					name: "Calculus - limits and continuity",
+					name: "2. Limits and continuity",
 					id: "PLJMXXdEk8kMDno0Eq0Ph7L7zOtNgYMqQn"
 				}
 			]
 		},
 		{
 			name: "Complex Analysis",
+			intro: "",
 			playlists: [
 				{
-					name: "Complex Analysis - Introduction and Topology",
+					name: "1. Introduction and Topology",
 					id: "PLJMXXdEk8kMBklpaHrBjQaKaCI2Vfxy7h"
 				},
 				{
-					name: "Complex Analysis - Elementary functions",
+					name: "2. Elementary functions",
 					id: "PLJMXXdEk8kMDETr7WetPdqF9GbQud6jDd"
 				}
 			]
 		},
 		{
 			name: "Miscellaneous",
+			intro: "Some web lectures on precalculus and on general mathematical background.",
 			playlists: [
 				{
 					name: "Algebra - factorization of polynomials",
@@ -57,7 +61,7 @@ var data = {
 
 var ViewModel = function(data) {
 	var self = this;
-    self.courses = ko.observableArray(data.courses);
+	self.courses = ko.observableArray(data.courses);
 	self.selectedCourse = ko.observable(null);
 	
 	self.selectHome = function() {
